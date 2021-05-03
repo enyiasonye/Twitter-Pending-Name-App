5b066f6377288dfa9fe689f170e7a47df091e428
Enyi Asonye <enyiasonye@gmail.com>
2021-05-03 11:27:58 -0500

    get rid of server direcrtory

--- server/models/tweet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const tweetSchema = new Schema({
-  content: {
-    type: [String],
-    required: [true, 'content is required'],
-  },
-});
-module.exports = mongoose.model('Tweet', tweetSchema, 'tweets');
